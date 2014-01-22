e74caac32c8b106ff8fe87b797266b6422814b97
Igor Bronovskyy <admin@brun.if.ua>
2014-01-22 16:40:06 +0200

    Template for site nung.edu.ua. Compleate 30%

---
+++ themes/nung/js/nung.js
@@ -0,0 +1,15 @@
+//
+// Developer: Igor Bronovskyy
+// Site: http://brun.if.ua
+//
+
+
+// Init
+$(document).ready(function() {
+	$('.dropdown-toggle').dropdown();
+
+	$('.carousel').carousel({
+		interval: 2000
+	})
+
+});
